d57a1693601f6a0108a480d2ca4a0ff1d75cf3df
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-05-18 17:33:38 -0700

    Updated UI

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 # File: main.py
 # Authors: Quinn Okabayashi, Theron Mansilla
 # Course: ENGR 27
-# Date: May 17 2021
+# Date: May 18 2021
 #
 ##############################################################################
 #
 # This file brings all the modules together, allowing users to interactively
 # to select existing datasets or create custom ones from videos automatically.
-# Then, they can view the individual images or stitch together a panorama.
+# Then, they can view the individual images or stitch together a panorama, 
+# as well as save the results to outputs/
 #
 ##############################################################################
 
@@ -19,34 +20,38 @@ import os
 import cv2
 from typing import List, Dict
 from demo import *
-from stitcher import *
+from stitcher import stitch
 
 
 def main():
-    DATASET = None
-    images: List[np.array] = []
-    cached: Dict[str, np.array] = {}
-
     while True:
-        images, DATASET = get_dataset_menu()
+        res: Union[None, Tuple[np.array, str]] = select_video_menu()
+        if res is None:
+            sys.exit(0)
+
+        images, dataset = res
 
         while True:
-            display_dataset_menu(DATASET)
+            display_dataset_menu(dataset)
+            stitched: Union[None, np.array] = None
 
             key = cv2.waitKey(0)
 
             if key == 27:
                 break
-            elif key == ord("1"):
+            elif key == ord('1'):
                 dataset_demo(images)
-            elif key == ord("2"):
-                # if DATASET not in cached.keys():
-                #     cached[DATASET] = stitch_many(images)
-
-                # result = cached[DATASET]
-                result = stitch_many(images)
-                cv2.imshow(WINDOW_NAME, result)
+            elif key == ord('2'):
+                if stitched is None:
+                    stitched = stitch(images)
+                cv2.imshow(WINDOW_NAME, stitched)
                 cv2.waitKey(0)
+            elif key == ord('3'):
+                # save stitched image
+                if stitched is None:
+                    stitched = stitch(images)
+                filename = f"{dataset}_{len(images)}.jpg"
+                cv2.imwrite(os.path.join('outputs', filename), stitched)
 
 
 if __name__ == "__main__":
